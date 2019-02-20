5ec2558bbb8fd8122a9f67062254ef41f1280b9c
Simon Davis <simon_freelance@hotmail.com>
2019-02-20 17:55:10 -0500

    Agent and Customer created with relationships between them.

---
+++ src/main/java/com/lambdaschool/orderssqlite/models/Agent.java
@@ -0,0 +1,31 @@
+package com.lambdaschool.orderssqlite.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@Table(name="agents")
+public class Agent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="agentcode", nullable = false)
+    private long id;
+
+    private String workingarea;
+
+    private double commission;
+
+    private String phone;
+
+    private String country;
+
+    @OneToMany(cascade = CascadeType.DETACH, mappedBy = "agent")
+    private Set<Customer> customers;
+
+    public Agent() {
+    }
+}
